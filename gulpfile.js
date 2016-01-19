14be6a997521b6e525896cbbcae2026ee73817f5
David Beeler <david.beeler@gmail.com>
2016-01-19 10:39:42 -0600

    boilerplate prep

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+
+// gulp.task(nameOfTaske, callback);
+// gulp.task('default', function() {
+//   console.log("logging out");
+// });
+
+gulp.task('default', function() {
+  return browserify('./source/app.js')
+        .transform("babelify", {presets: ["es2015", "react"]})
+        .bundle()
+        .pipe(source('build.js'))
+        .pipe(gulp.dest('./build/'))
+});
